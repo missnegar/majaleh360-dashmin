8de943147bb31a06e8464e6d9ddef5d5e0473dcc
Negar <33737965+missnegar@users.noreply.github.com>
2025-07-16 20:06:55 +0330

    Add vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
-import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react'; // این خط را اضافه می‌کنیم
+import path from 'path';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // پلاگین React را اینجا اضافه می‌کنیم
+      plugins: [react()], 
+      
+      // این خط مهم‌ترین بخش جدید است
+      base: '/majaleh360-dashmin/', 
+
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
